24beb41fc6ae20c42fe359d658e48e02af50dd3e
Samir Bouhassoun <samir.bouhassoun@sauermanngroup.com>
2024-10-15 17:05:00 +0200

    Added C convertor impl

---
+++ src/main/java/samir/kata/CConvertor.java
@@ -0,0 +1,12 @@
+package samir.kata;
+
+public class CConvertor extends Convertor {
+
+  public CConvertor() {
+    super();
+    super.numeralValue = 100;
+    super.romanValue = 'C';
+    super.previousValueThanSub = 'X';
+    super.valuePreviousValueThanSub = 10;
+  }
+}
