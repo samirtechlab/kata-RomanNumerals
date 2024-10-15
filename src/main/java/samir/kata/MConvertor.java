92a4dd268d36d5b15a0a6955bfd271c9d39287e1
Samir Bouhassoun <samir.bouhassoun@sauermanngroup.com>
2024-10-15 17:15:46 +0200

    Added M convertor impl

---
+++ src/main/java/samir/kata/MConvertor.java
@@ -0,0 +1,12 @@
+package samir.kata;
+
+public class MConvertor extends Convertor {
+
+  public MConvertor() {
+    super();
+    super.numeralValue = 1000;
+    super.romanValue = 'M';
+    super.previousValueThanSub = 'C';
+    super.valuePreviousValueThanSub = 100;
+  }
+}
