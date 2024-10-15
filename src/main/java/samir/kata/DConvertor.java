86f5abc309954f4335e023749a9660135e5c6d63
Samir Bouhassoun <samir.bouhassoun@sauermanngroup.com>
2024-10-15 17:10:32 +0200

    Added D convertor impl

---
+++ src/main/java/samir/kata/DConvertor.java
@@ -0,0 +1,12 @@
+package samir.kata;
+
+public class DConvertor extends Convertor {
+
+  public DConvertor() {
+    super();
+    super.numeralValue = 500;
+    super.romanValue = 'D';
+    super.previousValueThanSub = 'C';
+    super.valuePreviousValueThanSub = 100;
+  }
+}
